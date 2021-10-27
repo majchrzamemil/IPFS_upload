11c51dfc7fa44c76840c1b1d07df3661521c4355
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-10-27 14:07:15 +0200

    Initial reading file and pushing to IPFS
    Signed-off-by: Emil Majchrzak <majchrzamemil@github.com>

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use ipfs_api_backend_actix::{IpfsApi, IpfsClient};
+use std::env;
+use std::fs;
+use std::io::Cursor;
+
+#[actix_rt::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename: &String;
+    if args.len() > 1 {
+        filename = &args[1];
+        println!("{}", filename);
+    } else {
+        eprintln!("Provide filename as cmd arg!");
+        return;
+    }
+    let contents = fs::read_to_string(filename).expect("Unable to read file");
+    println!("Contents: {}", contents);
+    let data = Cursor::new(contents);
+
+    let client = IpfsClient::default();
+
+    match client.add(data).await {
+        Ok(res) => println!("{}", res.hash),
+        Err(e) => eprintln!("error adding file: {}", e),
+    }
+}
