40006bccffeac8f83b1ce53ae163b2bc6d51c11c
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-10-28 20:25:06 +0200

    MVP works if not run out of gas.
    There is still a lot of hardcoded values.
    Not splited to functions and no error handling.
    Take care of gas.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::env;
 use std::fs;
 use std::io::Cursor;
 
+use hex_literal::hex;
+
 #[actix_rt::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,9 +21,16 @@ async fn main() {
 
     let client = IpfsClient::default();
 
+    let cid: String;
     match client.add(data).await {
-        Ok(res) => println!("{}", res.hash),
-        Err(e) => eprintln!("error adding file: {}", e),
+        Ok(res) => {
+            println!("{}", res.hash);
+            cid = res.hash;
+        }
+        Err(e) => {
+            eprintln!("error adding file: {}", e);
+            return;
+        }
     }
 
     //WEB3 part
@@ -29,5 +38,28 @@ async fn main() {
 
     let transport = web3::transports::Http::new("http://localhost:8545").unwrap();
     let web3 = web3::Web3::new(transport);
+    //let contract_abi = "build/StoreCID.abi";
+    let contract_address: web3::types::H160 =
+        hex!("29cC4ecB8bbDBCF2d31bef45181c290952f2369B").into();
 
+    let contract = web3::contract::Contract::from_json(
+        web3.eth(),
+        contract_address,
+        include_bytes!("../build/StoreCID.abi"),
+    )
+    .unwrap();
+    let address_from = hex!("4beE06f55345dEB02119E39f4c791018F3FAb8A6").into();
+
+    println!("Publishing CID: {}", cid);
+
+    let result_hash = contract
+        .call(
+            "addCID",
+            cid,
+            address_from,
+            web3::contract::Options::default(),
+        )
+        .await
+        .unwrap();
+    println!("Transaction hash: {}", result_hash);
 }
