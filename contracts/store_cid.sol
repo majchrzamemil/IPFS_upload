53caab1b05c743256c6a35ffded37ad2e5567469
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-10-31 14:00:06 +0100

    Documented contract

--- contracts/store_cid.sol
+++ contracts/store_cid.sol
@@ -1,8 +1,11 @@
 pragma solidity 0.5.17;
 
+// Simplest possible contract for storring multiple CIDs
 contract StoreCID {
    
     // Dynamic array storring strings with CID
+    // Left public for test purpouses, in general
+    // it should be private.
     string[] public CIDs;
 
     // Adds new CID to contract
