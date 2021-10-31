2b44f6066ef7e08c6e7f896172c4270be76a332c
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-10-31 13:50:16 +0100

    Splited to functions + docs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,65 @@
+use hex_literal::hex;
 use ipfs_api_backend_actix::{IpfsApi, IpfsClient};
 use std::env;
 use std::fs;
 use std::io::Cursor;
 
-use hex_literal::hex;
+/// Returns CID of uploaded file
+///
+/// # Arguments
+///
+/// *'filename' - A name of a file which should be uploaded to IPFS
+async fn upload_to_ipsf(filename: &String) -> Option<String> {
+    let contents = fs::read(filename).expect("Unable to read file");
+    let data = Cursor::new(contents);
+
+    let client = IpfsClient::default();
+
+    match client.add(data).await {
+        Ok(res) => {
+            println!("Published CID: {}", res.hash);
+            Some(res.hash)
+        }
+        Err(e) => {
+            eprintln!("Error adding file: {}", e);
+            None
+        }
+    }
+}
+
+/// Calls smart contract and pushes CID. In this function there are multiple 'wild unwraps' and
+/// hardcoded values, due to low complexity, POC character and lack of specific requirements.
+///
+/// # Arguments
+///
+/// *'cid' - CID which sould be stored in hardcoded smart contract
+async fn push_cid(cid: String) {
+    let transport = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(transport);
+
+    // This field should be configurable, but due to lack of requirements left hardcoded
+    let contract_address: web3::types::H160 =
+        hex!("FadF67B8eB694977C4602A9bdda23E5F3Ab19EF1").into();
+
+    // There is a need to build StoreCID contract before running
+    let contract = web3::contract::Contract::from_json(
+        web3.eth(),
+        contract_address,
+        include_bytes!("../build/StoreCID.abi"),
+    )
+    .unwrap();
+
+    // This field should be configurable, but due to lack of requirements left hardcoded
+    let address_from = hex!("B9ce73A5CaA58aE1720A5529FaC1e306fD5EC827").into();
+
+    let mut transaction_options = web3::contract::Options::default();
+    transaction_options.gas = Some(web3::types::U256::from_dec_str("3000000").unwrap());
+    let result_hash = contract
+        .call("addCID", cid, address_from, transaction_options)
+        .await
+        .unwrap();
+    println!("Transaction hash: {}", result_hash);
+}
 
 #[actix_rt::main]
 async fn main() {
@@ -16,52 +72,11 @@ async fn main() {
         eprintln!("Provide filename as cmd arg!");
         return;
     }
-    let contents = fs::read(filename).expect("Unable to read file");
-    let data = Cursor::new(contents);
 
-    let client = IpfsClient::default();
-
-    let cid: String;
-    match client.add(data).await {
-        Ok(res) => {
-            println!("{}", res.hash);
-            cid = res.hash;
-        }
-        Err(e) => {
-            eprintln!("error adding file: {}", e);
+    match upload_to_ipsf(filename).await {
+        Some(cid) => push_cid(cid).await,
+        None => {
             return;
         }
     }
-
-    //WEB3 part
-    //To pass via env: http transport, account, smart contract address(might set defaults)
-
-    let transport = web3::transports::Http::new("http://localhost:8545").unwrap();
-    let web3 = web3::Web3::new(transport);
-    //let contract_abi = "build/StoreCID.abi";
-    let contract_address: web3::types::H160 =
-        hex!("FadF67B8eB694977C4602A9bdda23E5F3Ab19EF1").into();
-
-    let contract = web3::contract::Contract::from_json(
-        web3.eth(),
-        contract_address,
-        include_bytes!("../build/StoreCID.abi"),
-    )
-    .unwrap();
-    let address_from = hex!("B9ce73A5CaA58aE1720A5529FaC1e306fD5EC827").into();
-
-    println!("Publishing CID: {}", cid);
-
-    let mut transaction_options = web3::contract::Options::default();
-    transaction_options.gas = Some(web3::types::U256::from_dec_str("3000000").unwrap());
-    let result_hash = contract
-        .call(
-            "addCID",
-            cid,
-            address_from,
-            transaction_options,
-        )
-        .await
-        .unwrap();
-    println!("Transaction hash: {}", result_hash);
 }
